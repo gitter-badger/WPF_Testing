8884f416ab74134a216bd5e579c1067d0f87ba4f
tylersriver <tyler.w.sriver@eagles.oc.edu>
2016-05-31 22:57:37 -0500

    remove click

--- WpfApplication1/WpfApplication1/Lock Screen.xaml.cs
+++ WpfApplication1/WpfApplication1/Lock Screen.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfApplication1
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Lock_Screen.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Lock_Screen : Window
     {
-        public MainWindow()
+        public Lock_Screen()
         {
             InitializeComponent();
         }
